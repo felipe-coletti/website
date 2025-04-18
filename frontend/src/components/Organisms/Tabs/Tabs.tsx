bf5008b2ae37a5cc4566239d477ee3ec85bae3d7
felipe-coletti <felipecoletti2@gmail.com>
2025-04-18 17:45:30 -0300

    style(tabs): add active style

--- frontend/src/components/Organisms/Tabs/Tabs.tsx
+++ frontend/src/components/Organisms/Tabs/Tabs.tsx
@@ -1,16 +1,25 @@
+'use client'
 import { TabsProps } from './Tabs.types'
 import styles from './styles.module.css'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export const Tabs = ({ tabs }: TabsProps) => {
+    const pathname = usePathname()
+
     return (
         <ul className={styles.tabs}>
-            {tabs.map((tab, i) => (
-                <li>
-                    <a className={styles.tab} href={tab.href} key={i}>
-                        {tab.title}
-                    </a>
-                </li>
-            ))}
+            {tabs.map((tab, i) => {
+                const isActive = pathname === tab.href
+
+                return (
+                    <li key={i}>
+                        <Link className={`${styles.tab} ${isActive ? styles.active : ''}`} href={tab.href}>
+                            {tab.title}
+                        </Link>
+                    </li>
+                )
+            })}
         </ul>
     )
 }
