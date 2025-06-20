62542bcca26868dd06a703ecc436a9238f02c19a
felipe-coletti <felipecoletti2@gmail.com>
2025-06-20 19:13:14 -0300

    feat: add subheading for latest blog posts on Home page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,6 +4,7 @@ export const Home = () => {
 	return (
 		<PageWrapper className='page-list'>
 			<Heading>Welcome</Heading>
+			<Heading size='small'>Latest blog posts</Heading>
 		</PageWrapper>
 	)
 }
