da1b151b5f6a2015ad280cbd876b9a82f3a0ba61
felipe-coletti <felipecoletti2@gmail.com>
2025-06-23 14:39:24 -0300

    refactor: rename container class to pageWrapper

--- frontend/src/components/Organisms/PageWrapper/PageWrapper.tsx
+++ frontend/src/components/Organisms/PageWrapper/PageWrapper.tsx
@@ -3,7 +3,7 @@ import type { PageWrapperProps } from './PageWrapper.types'
 
 export const PageWrapper = ({ className, children }: PageWrapperProps) => {
 	return (
-		<div className={styles.container}>
+		<div className={styles.pageWrapper}>
 			<div className={`page-limit ${className}`}>{children}</div>
 		</div>
 	)
