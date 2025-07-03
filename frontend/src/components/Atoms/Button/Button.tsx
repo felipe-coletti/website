0d331911fbc910106842deac411133ba333b1606
felipe-coletti <felipecoletti2@gmail.com>
2025-07-03 12:58:06 -0300

    refactor(button): add icon suport and update variant names

--- frontend/src/components/Atoms/Button/Button.tsx
+++ frontend/src/components/Atoms/Button/Button.tsx
@@ -1,23 +1,33 @@
 import type { ButtonProps } from './Button.types'
 import styles from './styles.module.css'
 
-export const Button = ({ variant = 'filled', onClick, children }: ButtonProps) => {
-	let variantStyle
+export const Button = ({
+	variant = 'primary',
+	icon,
+	iconPosition = 'start',
+	onClick,
+	disabled = false,
+	children
+}: ButtonProps) => {
+	const variantStyle =
+		variant === 'secondary' ? styles.secondary : variant === 'tertiary' ? styles.tertiary : styles.primary
 
-	switch (variant) {
-		case 'filled':
-			variantStyle = styles.filled
-			break
-		case 'outline':
-			variantStyle = styles.outline
-			break
-		case 'ghost':
-			variantStyle = styles.ghost
-	}
+	const classNames = [
+		styles.button,
+		variantStyle,
+		icon && !children && styles.iconButton,
+		disabled && styles.disabled
+	]
+		.filter(Boolean)
+		.join(' ')
+
+	const renderIcon = () => icon && <span className={styles.icon}>{icon}</span>
 
 	return (
-		<button type='button' className={`${styles.button} ${variantStyle}`} onClick={onClick}>
+		<button type='button' className={classNames} onClick={onClick} disabled={disabled}>
+			{icon && iconPosition === 'start' && renderIcon()}
 			{children}
+			{icon && iconPosition === 'end' && renderIcon()}
 		</button>
 	)
 }
--- frontend/src/components/Atoms/Button/Button.types.ts
+++ frontend/src/components/Atoms/Button/Button.types.ts
@@ -1,5 +1,22 @@
-export interface ButtonProps {
-    variant?: 'filled' | 'outline' | 'ghost'
-    onClick?: () => void
-    children: React.ReactNode
+type CommonButtonProps = {
+	variant?: 'primary' | 'secondary' | 'tertiary'
+	iconPosition?: 'start' | 'end'
+	onClick?: () => void
+	disabled?: boolean
 }
+
+type IconOnlyProps = CommonButtonProps & {
+	icon: React.ReactNode
+	children?: never
+}
+type TextOnlyProps = CommonButtonProps & {
+	icon?: never
+	children: React.ReactNode
+}
+
+type IconTextProps = CommonButtonProps & {
+	icon: React.ReactNode
+	children: React.ReactNode
+}
+
+export type ButtonProps = IconOnlyProps | TextOnlyProps | IconTextProps
