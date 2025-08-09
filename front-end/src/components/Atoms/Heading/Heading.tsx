c41de049b1e2cb5fec1de968615020ccd1bc9671
felipe-coletti <felipecoletti@proton.me>
2025-08-09 00:48:13 -0300

    refactor(heading): simplify size style selection with object mapping

--- front-end/src/components/Atoms/Heading/Heading.tsx
+++ front-end/src/components/Atoms/Heading/Heading.tsx
@@ -2,24 +2,14 @@ import type { HeadingProps } from './Heading.types'
 import styles from './styles.module.css'
 
 export const Heading = ({ as: Tag, size = 'medium', children }: HeadingProps) => {
-	let sizeStyle
-
-	switch (size) {
-		case 'xsmall':
-			sizeStyle = styles.xsmall
-			break
-		case 'small':
-			sizeStyle = styles.small
-			break
-		case 'medium':
-			sizeStyle = styles.medium
-			break
-		case 'large':
-			sizeStyle = styles.large
-			break
-		case 'xlarge':
-			sizeStyle = styles.xlarge
-	}
+	const sizeStyle =
+		{
+			xsmall: styles.xsmall,
+			small: styles.small,
+			medium: styles.medium,
+			large: styles.large,
+			xlarge: styles.xlarge
+		}[size] || styles.medium
 
 	return <Tag className={`${styles.heading} ${sizeStyle}`}>{children}</Tag>
 }
